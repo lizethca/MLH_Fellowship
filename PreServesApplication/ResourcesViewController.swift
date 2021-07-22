576496399f11c77a3fe1993dc2a530eb66a45e48
Maggie Grether <maggie.grether@gmail.com>
2021-07-22 16:42:35 -0700

    Added text

---
+++ PreServesApplication/ResourcesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResourcesViewController.swift
+//  PreServesApplication
+//
+//  Created by Maggie Grether on 7/22/21.
+//
+
+import UIKit
+
+class ResourcesViewController: UIViewController {
+    @IBOutlet weak var globalFoodWasteLink: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateTextView ()
+
+    }
+    
+    func updateTextView() {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

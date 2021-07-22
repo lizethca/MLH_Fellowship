ab44ca5846ff76b09ed3d2ad3e9a8bd037f889d2
Maggie Grether <maggie.grether@gmail.com>
2021-07-21 23:09:34 -0700

    Added collection view, but couldn't run simulator. The label and image outlet wouldn't connect properly -- will ask in camp tomorrow

---
+++ CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  PreServesApplication
+//
+//  Created by Maggie Grether on 7/21/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var foodLabel: UILabel!
+    
+}
